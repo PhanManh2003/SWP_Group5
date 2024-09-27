0cacf260e512c4d10ad0ec02c7c92fcb74c816fe
Phan Manh <manhpthe172481@fpt.edu.vn>
2024-09-28 00:53:33 +0700

    login with GG

--- src/java/config/GlobalConfig.java
+++ src/java/config/GlobalConfig.java
@@ -9,20 +9,6 @@ package config;
  * @author manhpthe172481
  */
 public class GlobalConfig {
-    
-    /**
-     * SESSION_ACCOUNT: account
-     */
-    public static final String SESSION_ACCOUNT = "account";
 
-    /**
-     * USERNAME_EMAIL: isp1804cardweb3@gmail.com
-     */
-    public static final String USERNAME_EMAIL = "isp1804cardweb3@gmail.com";
 
-    /**
-     * PASSWORD_APP_EMAIL: pchn xlef opnr bhca
-     */
-    public static final String PASSWORD_APP_EMAIL = "pchn xlef opnr bhca";
-    
 }
